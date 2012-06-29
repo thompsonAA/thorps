bfdc5b94db2d07e29afd2bf43af1904bddaac136
Aaron Thompson <axelthompson12@gmail.com>
2012-06-28 19:46:48 -0700

    well this will be a simple song

--- app/assets/javascripts/bootstrap-tab.js
+++ app/assets/javascripts/bootstrap-tab.js
@@ -2,11 +2,3 @@ $('#myTab a').click(function (e) {
   e.preventDefault();
   $(this).tab('show');
 })
-
-
-$('#myTab a[href="#home"]').tab('show');
-$('#myTab a[href="#tab2"]').tab('show');
-$('#myTab a[href="#tab3"]').tab('show');
-$('#myTab a[href="#tab4"]').tab('show');
-$('#myTab a[href="#tab5"]').tab('show');
-
